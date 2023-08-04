2a5a71418653b1e9ec0ef9aa6464ed0046d53c64
Helmy Kambuna Otaya (Tokko Blogger) <21377149+twinnybloggers@users.noreply.github.com>
2023-08-04 15:13:10 +0700

    Update firebase.init.js

--- Scripts/firebase.init.js
+++ Scripts/firebase.init.js
@@ -1,12 +1,14 @@
 ﻿// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyBxhkOKc_tWaeeUSePIwitc2Tm-aqrLTww",
-    authDomain: "chat-app-7da13.firebaseapp.com",
-    databaseURL: "https://chat-app-7da13.firebaseio.com",
-    projectId: "chat-app-7da13",
-    storageBucket: "",
-    messagingSenderId: "454878238535",
-    appId: "1:454878238535:web:6fb06475d86f38e4"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC-SEma3JShAJXe4h47x9nWPMRf8aF9e7M",
+  authDomain: "tokko-login.firebaseapp.com",
+  databaseURL: "https://tokko-login-default-rtdb.firebaseio.com",
+  projectId: "tokko-login",
+  storageBucket: "tokko-login.appspot.com",
+  messagingSenderId: "604336992026",
+  appId: "1:604336992026:web:8091ec5a0b8a1a5d15c6a3",
+  measurementId: "G-VYVZGNYLC3"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
